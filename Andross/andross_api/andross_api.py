c1eaf224a6e9726878bbbdc249caf66462995fc3
Soph <consts7s@gmail.com>
2023-06-12 06:36:42 -0400

    Changed name of API_KEY environment variable to not cause issues when copying to /etc/environment

--- Andross/andross_api/andross_api.py
+++ Andross/andross_api/andross_api.py
@@ -3,7 +3,7 @@ from os import getenv
 from ratelimiter import RateLimiter
 import requests
 
-api_key = getenv('API-KEY')
+api_key = getenv('API_KEY')
 api_url = 'http://' + getenv('API_URL')
 authorization_header = {"X-API-KEY": api_key, 'Content-Type': 'application/json'}
 
